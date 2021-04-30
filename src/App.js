9e4f6f95b2a9bb92b47976c751dc250b5279d8c3
AdityaChondke <aditya.chondke@gmail.com>
2021-04-30 20:49:56 +0530

    Toggle Buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react'
 
 function App() {
+  const [show,setShow]=useState(false);
+  const [showR,setShowR]=useState(false);
   return (
     <div className="App">
-     <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script><iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrKDaeI6Rm0w7waP?backgroundColor=yellow" frameborder="0" onmousewheel="" width="100%" height="1011" styles="background: transparent; border: 1px solid #ccc;"/>
+      <div className="Box">
+      <div className="Left">
+        <button onClick={()=>setShow(!show)}>Doante Plasma</button>
+     { 
+     
+     show?<iframe class="airtable-embed" src="https://airtable.com/embed/shrKDaeI6Rm0w7waP?backgroundColor=yellow" 
+     frameborder="0" onmousewheel="" width="100%" height="533" 
+     styles="background: transparent; border: 1px solid #ccc;"/>:null
+     
+     
+     }
      </div>
+     <div className="Right">
+        <button onClick={()=>setShowR(!showR)}>Request Plasma</button>
+     { 
+     
+     showR?<iframe class="airtable-embed" src="https://airtable.com/embed/shr4Rw1FTBD3VBOO1?backgroundColor=yellow" 
+     frameborder="0" onmousewheel="" width="100%" height="533" 
+     styles="background: transparent; border: 1px solid #ccc;"/>:null
+     
+     
+     }
+     </div>
+      </div>
+
+    </div>
   );
 }
 
