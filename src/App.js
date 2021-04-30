2b53ada8ed74ef8e1343b89597b19b6e53aee0e6
AdityaChondke <aditya.chondke@gmail.com>
2021-04-30 20:11:09 +0530

    Form Optimized

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <iframe class="airtable-embed" src="https://airtable.com/embed/shrKDaeI6Rm0w7waP?backgroundColor=yellow" frameborder="0" onmousewheel="" width="100%" height="533" styles="background: transparent; border: 1px solid #ccc;"/>
-      </div>
+     <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script><iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrKDaeI6Rm0w7waP?backgroundColor=yellow" frameborder="0" onmousewheel="" width="100%" height="1011" styles="background: transparent; border: 1px solid #ccc;"/>
+     </div>
   );
 }
 
