5463afc00f61c38d9db9e72b28bb3faaca15a37b
AdityaChondke <aditya.chondke@gmail.com>
2021-04-30 20:08:15 +0530

    Test Form Added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <iframe class="airtable-embed" src="https://airtable.com/embed/shrKDaeI6Rm0w7waP?backgroundColor=yellow" frameborder="0" onmousewheel="" width="100%" height="533" styles="background: transparent; border: 1px solid #ccc;"/>;
+     <iframe class="airtable-embed" src="https://airtable.com/embed/shrKDaeI6Rm0w7waP?backgroundColor=yellow" frameborder="0" onmousewheel="" width="100%" height="533" styles="background: transparent; border: 1px solid #ccc;"/>
       </div>
   );
 }
