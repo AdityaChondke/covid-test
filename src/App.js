e5f3e850e3eebf82fe98b4b4df76490788419a82
AdityaChondke <aditya.chondke@gmail.com>
2021-05-01 20:32:04 +0530

    Updated Cards UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import './App.css';
 import React,{useState} from 'react'
-import Button from 'react-bootstrap/Button';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import {  ButtonToolbar, Button,Card } from 'react-bootstrap'
 
 function App() {
   const [show,setShow]=useState(false);
   const [showR,setShowR]=useState(false);
+    
+  
   return (
     <div className="App">
-      <ButtonToolbar className="custom-btn-toolbar">
-      <Button onClick={()=>{setShow(!show); setShowR(false)}}>{show?'Close Donation Form':'Donate Plasma'}</Button>
-      <Button onClick={()=>{setShowR(!showR); setShow(false)}}>{showR?'Close Request Form':'Request Plasma'}</Button>
-      </ButtonToolbar>
+      <img src="/images/plasma.jpg" />
+      <Card className="text-center"  >
+      
+        <Card.Header>Yavatmal</Card.Header>
+        <Card.Body>
+          <Card.Title>Plasma Donation and Request</Card.Title>
+          <Card.Text>
+            Fill one of these forms.
+            Our team will match relevant donors and patients and put them in touch with each other.
+            We are a group of volunteers helping our city get over this COVID-19 crisis.
+          </Card.Text>
+          <ButtonToolbar className="custom-btn-toolbar">
+            <Button onClick={()=>{setShow(!show); setShowR(false)}}>{show?'Close Donation Form':'Donate Plasma'}</Button>
+            <Button onClick={()=>{setShowR(!showR); setShow(false)}}>{showR?'Close Request Form':'Request Plasma'}</Button>
+          </ButtonToolbar>
+        </Card.Body>
+        
+      </Card>
+     
         <div className="Box">
+        
+              
               { 
-              show?<iframe class="airtable-embed" src="https://airtable.com/embed/shrKDaeI6Rm0w7waP?backgroundColor=yellow" 
+              show?
+              <div>
+              <iframe class="airtable-embed" src="https://airtable.com/embed/shr6Pf2Dc3odgIVpc?backgroundColor=yellow" 
               frameborder="0" onmousewheel="" width="100%" height="533" 
-              styles="background: transparent; border: 1px solid #ccc;"/>:null }
+              styles="background: transparent; border: 1px solid #ccc;">
+              </iframe>
+              </div>:null }
             {
-            showR?<iframe class="airtable-embed" src="https://airtable.com/embed/shr4Rw1FTBD3VBOO1?backgroundColor=yellow" 
+            showR?
+            <div>
+            <iframe class="airtable-embed" src="https://airtable.com/embed/shrlPtj561qqfHLok?backgroundColor=yellow" 
             frameborder="0" onmousewheel="" width="100%" height="533" 
-            styles="background: transparent; border: 1px solid #ccc;"/>:null
+            styles="background: transparent; border: 1px solid #ccc;">
+            </iframe>
+            </div>:null
             }
           
         </div>
