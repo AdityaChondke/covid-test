96113d5c27886a2559353c6ce7b599326dbdef50
AdityaChondke <aditya.chondke@gmail.com>
2021-05-02 18:59:42 +0530

    Updated Graphics

--- src/App.js
+++ src/App.js
@@ -1,55 +1,60 @@
 import './App.css';
 import React,{useState} from 'react'
-import {  ButtonToolbar, Button,Card } from 'react-bootstrap'
+import {  ButtonToolbar, Button,Card,Navbar,Nav } from 'react-bootstrap'
+import plasma from './plasma.gif';
 
 function App() {
   const [show,setShow]=useState(false);
   const [showR,setShowR]=useState(false);
     
-  
   return (
     <div className="App">
-      <img src="/images/plasma.jpg" />
-      <Card className="text-center"  >
       
-        <Card.Header>Yavatmal</Card.Header>
+      <Navbar className="color-nav" variant="dark">
+    <Navbar.Brand href="/covid-test">Yavatmal-Cares</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link >Home</Nav.Link>
+      </Nav>
+    </Navbar>
+
+      <Card className="text-center">
+      <Card.Img variant="top" src={plasma} alt="test" />
         <Card.Body>
           <Card.Title>Plasma Donation and Request</Card.Title>
           <Card.Text>
             Fill one of these forms.
-            Our team will match relevant donors and patients and put them in touch with each other.
-            We are a group of volunteers helping our city get over this COVID-19 crisis.
           </Card.Text>
+          <Card.Text>
+          Our team will match relevant donors and patients and put them in touch with each other.
+          </Card.Text>
+          <Card.Text> We are a group of volunteers helping our city get over this COVID-19 crisis.</Card.Text>
+
           <ButtonToolbar className="custom-btn-toolbar">
             <Button onClick={()=>{setShow(!show); setShowR(false)}}>{show?'Close Donation Form':'Donate Plasma'}</Button>
             <Button onClick={()=>{setShowR(!showR); setShow(false)}}>{showR?'Close Request Form':'Request Plasma'}</Button>
           </ButtonToolbar>
         </Card.Body>
-        
       </Card>
      
         <div className="Box">
-        
-              
               { 
               show?
-              <div>
               <iframe class="airtable-embed" src="https://airtable.com/embed/shr6Pf2Dc3odgIVpc?backgroundColor=yellow" 
               frameborder="0" onmousewheel="" width="100%" height="533" 
               styles="background: transparent; border: 1px solid #ccc;">
               </iframe>
-              </div>:null }
-            {
-            showR?
-            <div>
-            <iframe class="airtable-embed" src="https://airtable.com/embed/shrlPtj561qqfHLok?backgroundColor=yellow" 
-            frameborder="0" onmousewheel="" width="100%" height="533" 
-            styles="background: transparent; border: 1px solid #ccc;">
-            </iframe>
-            </div>:null
-            }
-          
+              :null}
+              {
+              showR?
+              <iframe class="airtable-embed" src="https://airtable.com/embed/shrlPtj561qqfHLok?backgroundColor=yellow" 
+              frameborder="0" onmousewheel="" width="100%" height="533" 
+              styles="background: transparent; border: 1px solid #ccc;"/>:null
+              } 
         </div>
+        
+    
+     
+    
     </div>
   );
 }
